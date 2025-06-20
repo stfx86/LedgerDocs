de135b40c0dc103b1bd41bbc74c090e6bed75752
stfx <mustaphabennasser8@gmail.com>
2025-06-20 18:53:01 +0100

    fine util now

---
+++ ignition/modules/LedgerDocModule_copy.js
@@ -0,0 +1,15 @@
+// ignition/modules/LedgerDocModule.js
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+const { execSync } = require("child_process");
+// const {truncateTables} =require("../../backend/src/utils/truncateTables")
+
+module.exports = buildModule("LedgerDocModule", (m) => {
+  // Deploy the LedgerDoc contract
+
+  const ledgerDoc = m.contract("LedgerDoc");
+
+
+  //
+  // truncateTables();
+  return { ledgerDoc };
+});
--- scripts/getUser.js
+++ scripts/getUser.js
@@ -2,11 +2,11 @@
 
     async function main() {
         const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // Replace with actual address
-        const userAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"; // Replace with target wallet
+        const userAddress = "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199"; // Replace with target wallet
 
         const LedgerDoc = await ethers.getContractAt("LedgerDoc", contractAddress);
         const user = await LedgerDoc.getUserByAddress(userAddress);
-        new ethers.wallet()
+
 
         console.log(user);
     }
